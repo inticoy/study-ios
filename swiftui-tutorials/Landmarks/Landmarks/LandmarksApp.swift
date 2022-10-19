05ab96ea85e8c44fdad15b3f1d1a116393176930
inticoy <inticoy0406@gmail.com>
2022-10-19 18:57:58 +0900

    chapter1 Creating and Combining Views

---
+++ swiftui-tutorials/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Inticoy on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
